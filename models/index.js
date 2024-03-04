b80bd714208493136dd004683ce9775f8e414259
jinhyeong830 <shg02387@naver.com>
2024-03-04 13:18:43 +0900

    fc

---
+++ models/index.js
@@ -0,0 +1,21 @@
+'use strict';
+const Sequelize = require('sequelize');
+// console.log(process.env.DB_MODE);
+console.log(process.env.NODE_ENV);
+
+let config;
+if (process.env.NODE_ENV) {
+  config = require(__dirname + '/../config/config.js')[process.env.NODE_ENV];
+} else {
+  config = require(__dirname + '/../config/config.js')['development'];
+}
+console.log(config);
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.Comment = require('./Comment')(sequelize, Sequelize);
+
+module.exports = db;
